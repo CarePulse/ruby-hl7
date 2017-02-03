13536936802fa07b16e1efbab6efb358860cde86
Bill Barnett <agilous@users.noreply.github.com>
2017-02-03 14:15:14 -0500

    Fix order of bed states

--- lib/segments/npu.rb
+++ lib/segments/npu.rb
@@ -2,7 +2,7 @@
 class HL7::Message::Segment::NPU < HL7::Message::Segment
   weight 2
   add_field :location
-  add_field :old_bed_status
   add_field :new_bed_status
+  add_field :old_bed_status
   add_field :room_comment
 end
